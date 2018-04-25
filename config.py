3b812d49a9e65f9542cb51433e099fbbf9adfc3c
esensar <esarajcic1@etf.unsa.ba>
2018-04-25 15:51:28 +0200

    Embed simple MQTT client in the application

---
+++ config.py
@@ -0,0 +1,5 @@
+MQTT_BROKER_URL = 'mybroker.com'
+MQTT_BROKER_PORT = 1883
+MQTT_USERNAME = 'user'
+MQTT_PASSWORD = 'secret'
+MQTT_REFRESH_TIME = 1.0  # refresh time in seconds
--- hello.py
+++ hello.py
@@ -1,6 +1,23 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_mqtt import Mqtt
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+mqtt = Mqtt(app)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('topic/state')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print(message.payload.decode())
