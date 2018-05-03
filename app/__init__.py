f4e2ace7b00d3fa2322c39b10eea7b71acf94f3e
esensar <esarajcic1@etf.unsa.ba>
2018-05-03 20:55:16 +0200

    Make config loading silent

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = FlaskAPI(__name__, instance_relative_config=True)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py', silent=True)
 db = SQLAlchemy(app)
 
 
