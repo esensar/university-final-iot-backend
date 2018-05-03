23303bdb3f91975bccce4601f796fd54b024c228
esensar <esarajcic1@etf.unsa.ba>
2018-05-03 17:01:32 +0200

    Change response to JSON

--- app/mod_accounts/controllers.py
+++ app/mod_accounts/controllers.py
@@ -1,17 +1,20 @@
 from app import db
-from flask import request, current_app
+from flask import request, jsonify, abort
 from .models import Account, Role
 
 def initialize_routes(accounts):
     @accounts.route("/", methods=['POST'])
     def create():
-        with current_app.app_context():
-            print(request.data)
-            user = request.data.get('user')
-            acct = Account(user.get('username'),
-                           user.get('password'),
-                           user.get('email'),
-                           2)
-            acct.save()
-            return "Success!"
+        print(request.data)
+        user = request.data.get('user')
+        acct = Account(user.get('username'),
+                       user.get('password'),
+                       user.get('email'),
+                       2)
+        acct.save()
+        response = jsonify({
+            'message':'Success!'
+            })
+        response.status_code = 200
+        return response
 
