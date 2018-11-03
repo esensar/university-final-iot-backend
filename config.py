2b0273cd934883314ed356fcd2a5193fb5e1b0f7
Ensar Sarajčić <esarajcic1@etf.unsa.ba>
2018-11-03 18:25:52 +0100

    Version 0.4.2

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # App configuration
 DEBUG = os.environ['DEBUG']
-APP_VERSION = '0.4.1'
+APP_VERSION = '0.4.2'
 
 # Define the application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
